6d16f67e95b73ebad2e720d5ccb159fcc2519505
paypur <pieceofpaypur@gmail.com>
2024-08-20 18:54:43 -0700

    Added * Date column
    Took 1 hour

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
+use chrono::Local;
 use mpris::{Event, PlayerFinder};
-use rusqlite::{Connection};
+use rusqlite::Connection;
 
 const FILE_NAME: &str = "mpressed.db";
 
 #[derive(Debug)]
 struct Song {
+    date: String,
     artist: String,
     album: String,
     title: String,
-    count: u32
+    count: u32,
 }
 
 fn main() {
+
     let db = Connection::open(FILE_NAME).unwrap();
 
-    &db.execute("CREATE TABLE if not exists song_data (
+    &db.execute(
+        "CREATE TABLE if not exists song_data (
+                        date TEXT,
                         artist TEXT,
                         album TEXT,
                         title TEXT,
                         count INTEGER
-                    )", [])
-        .expect("Failed to create table");
+                    )",
+        [],
+    )
+    .expect("Failed to create table");
 
-
-    write(&db);
+    event_loop(&db);
 
     db.close().unwrap();
 }
 
-fn write(db: &Connection) {
+fn event_loop(db: &Connection) {
     let player = PlayerFinder::new()
         .expect("Could not connect to D-Bus")
         .find_by_name("VLC media player")
@@ -46,36 +52,37 @@ fn write(db: &Connection) {
         match event {
             Ok(event) => match event {
                 Event::TrackChanged(data) => {
-                    let mut song = Song {
-                        artist: data.artists().unwrap()[0].to_string(),
+                    let song = Song {
+                        date: Local::now().date_naive().to_string(),
+                        artist: data.artists().unwrap().join(" / "),
                         album: data.album_name().unwrap().to_string(),
                         title: data.title().unwrap().to_string(),
                         count: 1,
                     };
 
-                    let mut statement = db.prepare("SELECT * FROM song_data WHERE artist = (?1) AND album = (?2) AND title = (?3) LIMIT 1").unwrap();
-                    let query = statement.query((&song.artist, &song.album, &song.title));
+                    let mut statement = db.prepare("SELECT * FROM song_data WHERE date = (?1) AND artist = (?2) AND album = (?3) AND title = (?4) LIMIT 1").unwrap();
+                    let query = statement.query((&song.date, &song.artist, &song.album, &song.title));
 
                     let mut is_empty = true;
 
-                    while let Some (row) = query.unwrap().next().unwrap() {
+                    while let Some(row) = query.unwrap().next().unwrap() {
                         is_empty = false;
-                        song.count += row.get::<usize, u32>(3).unwrap();
+                        // song.count += row.get::<usize, u32>(3).unwrap();
                         break;
                     }
 
                     if is_empty {
-                        db.execute("INSERT INTO song_data (artist, album, title, count) VALUES (?1, ?2, ?3, ?4)",
-                                   (&song.artist, &song.album, &song.title, &song.count))
-                            .expect(format!("Failed to insert record {:?}", (&song.artist, &song.album, &song.title, &song.count)).as_str());
+                        db.execute("INSERT INTO song_data (date, artist, album, title, count) VALUES (?1, ?2, ?3, ?4, ?5)",
+                                   (&song.date, &song.artist, &song.album, &song.title, &song.count))
+                            .expect(format!("Failed to insert record {:?}", (&song.date, &song.artist, &song.album, &song.title, &song.count)).as_str());
                     } else {
-                        db.execute("UPDATE song_data SET count = count + 1 WHERE artist = (?1) AND album = (?2) AND title = (?3)",
-                                   (&song.artist, &song.album, &song.title))
-                            .expect(format!("Failed to update record {:?}", (&song.artist, &song.album, &song.title, &song.count)).as_str());
+                        db.execute("UPDATE song_data SET count = count + 1 WHERE date = (?1) AND artist = (?2) AND album = (?3) AND title = (?4)",
+                                   (&song.date, &song.artist, &song.album, &song.title))
+                            .expect(format!("Failed to update record {:?}", (&song.date, &song.artist, &song.album, &song.title)).as_str());
                     }
                 },
                 _ => (),
-            }
+            },
             Err(err) => {
                 println!("D-Bus error: {}. Aborting.", err);
                 break;
@@ -85,4 +92,3 @@ fn write(db: &Connection) {
 
     println!("Event stream ended.");
 }
-

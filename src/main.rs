186a6f76f8d2219ea183073425f582677c2cd02d
paypur <pieceofpaypur@gmail.com>
2024-08-20 22:57:25 -0700

    Added * Minimum playtime check * Debug printing
    Changed
    * Database logic
    
    Took 2 hours

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use mpris::{Event, PlayerFinder};
 use rusqlite::Connection;
 
 const FILE_NAME: &str = "mpressed.db";
+const MIN_PLAYTIME: u8 = 10;
 
 #[derive(Debug)]
 struct Song {
@@ -14,20 +15,16 @@ struct Song {
 }
 
 fn main() {
-
     let db = Connection::open(FILE_NAME).unwrap();
 
-    &db.execute(
-        "CREATE TABLE if not exists song_data (
+    &db.execute("CREATE TABLE if not exists song_data (
                         date TEXT,
                         artist TEXT,
                         album TEXT,
                         title TEXT,
                         count INTEGER
-                    )",
-        [],
-    )
-    .expect("Failed to create table");
+                    )", [])
+        .expect("Failed to create table");
 
     event_loop(&db);
 
@@ -41,46 +38,58 @@ fn event_loop(db: &Connection) {
         // .find_active()
         .expect("Could not find active player");
 
-    println!(
-        "Showing event stream for player {}...\n(Exit with Ctrl-C)\n",
-        player.identity()
-    );
+    println!("Showing event stream for player {}...\n(Exit with Ctrl-C)\n", player.identity());
 
-    let events = player.events().expect("Could not start event stream");
+    let mut track_last_changed: i64 = 0;
+    let mut song_option: Option<Song> = None;
 
-    for event in events {
+    for event in player.events().expect("Could not start event stream") {
         match event {
             Ok(event) => match event {
                 Event::TrackChanged(data) => {
-                    let song = Song {
-                        date: Local::now().date_naive().to_string(),
-                        artist: data.artists().unwrap().join(" / "),
-                        album: data.album_name().unwrap().to_string(),
-                        title: data.title().unwrap().to_string(),
-                        count: 1,
-                    };
+                    let current_date = Local::now().date_naive().to_string();
+                    let current_time = Local::now().timestamp();
 
-                    let mut statement = db.prepare("SELECT * FROM song_data WHERE date = (?1) AND artist = (?2) AND album = (?3) AND title = (?4) LIMIT 1").unwrap();
-                    let query = statement.query((&song.date, &song.artist, &song.album, &song.title));
-
-                    let mut is_empty = true;
-
-                    while let Some(row) = query.unwrap().next().unwrap() {
-                        is_empty = false;
-                        // song.count += row.get::<usize, u32>(3).unwrap();
-                        break;
-                    }
-
-                    if is_empty {
-                        db.execute("INSERT INTO song_data (date, artist, album, title, count) VALUES (?1, ?2, ?3, ?4, ?5)",
-                                   (&song.date, &song.artist, &song.album, &song.title, &song.count))
-                            .expect(format!("Failed to insert record {:?}", (&song.date, &song.artist, &song.album, &song.title, &song.count)).as_str());
+                    if current_time - track_last_changed > MIN_PLAYTIME as i64 {
+                        match song_option {
+                            Some(song) => {
+                                match &db.execute("UPDATE song_data SET count = count + 1 WHERE date = (?1) AND artist = (?2) AND album = (?3) AND title = (?4)",
+                                                  (&song.date, &song.artist, &song.album, &song.title)) {
+                                    Ok(updated) => {
+                                        if *updated == 1 {
+                                            println!("Updated record: {:?}", (&song.date, &song.artist, &song.album, &song.title));
+                                        } else {
+                                            match &db.execute("INSERT INTO song_data (date, artist, album, title, count) VALUES (?1, ?2, ?3, ?4, ?5)",
+                                                              (&song.date, &song.artist, &song.album, &song.title, &song.count)) {
+                                                Ok(_) => println!("Inserted record: {:?}", (&song.date, &song.artist, &song.album, &song.title)),
+                                                Err(_) => println!("Failed to insert record: {:?}", (&song.date, &song.artist, &song.album, &song.title)),
+                                            }
+                                        }
+                                    }
+                                    Err(_) => {
+                                        println!("Failed to update record: {:?}", (&song.date, &song.artist, &song.album, &song.title));
+                                    }
+                                }
+                            }
+                            None => (),
+                        }
                     } else {
-                        db.execute("UPDATE song_data SET count = count + 1 WHERE date = (?1) AND artist = (?2) AND album = (?3) AND title = (?4)",
-                                   (&song.date, &song.artist, &song.album, &song.title))
-                            .expect(format!("Failed to update record {:?}", (&song.date, &song.artist, &song.album, &song.title)).as_str());
+                        let song = song_option.unwrap();
+                        println!("Song skipped: {:?}. Minimum playtime ({}s) not met.", (&song.date, &song.artist, &song.album, &song.title), MIN_PLAYTIME);
                     }
-                },
+
+                    track_last_changed = current_time;
+
+                    song_option = Some(
+                        Song {
+                            date: current_date,
+                            artist: data.artists().unwrap().join(" / "),
+                            album: data.album_name().unwrap().to_string(),
+                            title: data.title().unwrap().to_string(),
+                            count: 1,
+                        }
+                    );
+                }
                 _ => (),
             },
             Err(err) => {

b5e7e09c0d21757d708b7b2dbbea694e74a76884
paypur <pieceofpaypur@gmail.com>
2024-08-24 00:13:37 -0400

    Changed * Database structure
    Took 2 hours 20 minutes

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 use chrono::Local;
 use mpris::{Event, PlayerFinder};
-use rusqlite::Connection;
+use rusqlite::{Connection};
 
 const FILE_NAME: &str = "mpressed.db";
 const MIN_PLAYTIME: u8 = 10;
 
 #[derive(Debug)]
-struct Song {
-    date: String,
+struct SongData {
     artist: String,
     album: String,
     title: String,
-    count: u32,
 }
 
 fn main() {
     let db = Connection::open(FILE_NAME).unwrap();
 
     &db.execute("CREATE TABLE if not exists song_data (
-                        date TEXT,
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                         artist TEXT,
                         album TEXT,
                         title TEXT,
-                        count INTEGER
+                        UNIQUE(artist, album, title)
                     )", [])
-        .expect("Failed to create table");
+        .expect("Failed to create song_data table");
+
+    &db.execute("CREATE TABLE if not exists song_plays (
+                        id INTEGER,
+                        date TEXT,
+                        plays INTEGER,
+                        UNIQUE(id, date)
+                    )", [])
+        .expect("Failed to create song_plays table");
 
     event_loop(&db);
 
@@ -41,7 +47,7 @@ fn event_loop(db: &Connection) {
     println!("Showing event stream for player {}...\n(Exit with Ctrl-C)\n", player.identity());
 
     let mut track_last_changed: i64 = 0;
-    let mut song_option: Option<Song> = None;
+    let mut song_option: Option<SongData> = None;
 
     for event in player.events().expect("Could not start event stream") {
         match event {
@@ -53,21 +59,35 @@ fn event_loop(db: &Connection) {
                     if current_time - track_last_changed > MIN_PLAYTIME as i64 {
                         match song_option {
                             Some(song) => {
-                                match &db.execute("UPDATE song_data SET count = count + 1 WHERE date = (?1) AND artist = (?2) AND album = (?3) AND title = (?4)",
-                                                  (&song.date, &song.artist, &song.album, &song.title)) {
-                                    Ok(updated) => {
-                                        if *updated == 1 {
-                                            println!("Updated record: {:?}", (&song.date, &song.artist, &song.album, &song.title));
+                                match db.execute("INSERT OR IGNORE INTO song_data (artist, album, title) VALUES (?1, ?2, ?3)",
+                                                 (&song.artist, &song.album, &song.title)) {
+                                    Ok(_) => println!("Inserted song_data: {:?}", (&song.artist, &song.album, &song.title)),
+                                    Err(_) => println!("Failed to insert song_data: {:?}", (&song.artist, &song.album, &song.title)),
+                                }
+
+                                let mut statement = db.prepare("SELECT ID FROM song_data WHERE artist = (?1) AND album = (?2) AND title = (?3) LIMIT 1")
+                                    .unwrap();
+                                let mut query = statement.query((&song.artist, &song.album, &song.title))
+                                    .unwrap();
+                                let row = query.next().unwrap();
+                                let id: u32 = row.unwrap().get(0).unwrap();
+
+                                match &db.prepare("UPDATE song_plays SET plays = plays + 1 WHERE id = (?1) AND date = (?2)")
+                                    .unwrap()
+                                    .execute((id, &current_date)) {
+                                    Ok(update) => {
+                                        if *update == 1 {
+                                            println!("Updated song_plays: {:?}", (&song.artist, &song.album, &song.title));
                                         } else {
-                                            match &db.execute("INSERT INTO song_data (date, artist, album, title, count) VALUES (?1, ?2, ?3, ?4, ?5)",
-                                                              (&song.date, &song.artist, &song.album, &song.title, &song.count)) {
-                                                Ok(_) => println!("Inserted record: {:?}", (&song.date, &song.artist, &song.album, &song.title)),
-                                                Err(_) => println!("Failed to insert record: {:?}", (&song.date, &song.artist, &song.album, &song.title)),
+                                            match &db.execute("INSERT INTO song_plays (id, date, plays) VALUES (?1, ?2, ?3)",
+                                                              (id, &current_date, 1)) {
+                                                Ok(_) => println!("Inserted song_plays: {:?}", (&song.artist, &song.album, &song.title)),
+                                                Err(_) => println!("Failed to insert song_plays: {:?}", (&song.artist, &song.album, &song.title)),
                                             }
                                         }
                                     }
                                     Err(_) => {
-                                        println!("Failed to update record: {:?}", (&song.date, &song.artist, &song.album, &song.title));
+                                        println!("Failed to update song_plays: {:?}", (&song.artist, &song.album, &song.title));
                                     }
                                 }
                             }
@@ -75,18 +95,16 @@ fn event_loop(db: &Connection) {
                         }
                     } else {
                         let song = song_option.unwrap();
-                        println!("Song skipped: {:?}. Minimum playtime ({}s) not met.", (&song.date, &song.artist, &song.album, &song.title), MIN_PLAYTIME);
+                        println!("Skipped song: {:?}, minimum playtime ({}s) not met.", (&song.artist, &song.album, &song.title), MIN_PLAYTIME);
                     }
 
                     track_last_changed = current_time;
 
                     song_option = Some(
-                        Song {
-                            date: current_date,
+                        SongData {
                             artist: data.artists().unwrap().join(" / "),
                             album: data.album_name().unwrap().to_string(),
                             title: data.title().unwrap().to_string(),
-                            count: 1,
                         }
                     );
                 }
